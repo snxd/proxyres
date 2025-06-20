db4146d225996d8367341d5a6f57a8e52dda5e24
Steve Tucker <stevet@solidstatenetworks.com>
2025-06-19 18:46:21 -0700

    Move log declarations into extern "C" scope.

--- log.h
+++ log.h
@@ -3,14 +3,14 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void proxy_log_set_error_cb(void (*func)(const char *fmt, va_list args));
 void proxy_log_set_warn_cb(void (*func)(const char *fmt, va_list args));
 void proxy_log_set_info_cb(void (*func)(const char *fmt, va_list args));
 void proxy_log_set_debug_cb(void (*func)(const char *fmt, va_list args));
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 void log_error(const char *fmt, ...);
 void log_warn(const char *fmt, ...);
