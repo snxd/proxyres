23ef1963604d31f25a7fe7d0b6cfe71b574f3595
Nathan Moinvaziri <nathan@nathanm.com>
2026-02-23 13:10:26 -0800

    Remove old WPAD/DHCP test harness.

--- test/dhcp_server_cmd.js
+++
@@ -1,8 +0,0 @@
-const { spawn } = require("child_process");
-const os = require("os");
-
-if (os.platform() === "win32") {
-  spawn("yarn", ["run", "dhcp_server"], { stdio: "inherit", shell: true });
-} else {
-  spawn("sudo", ["yarn", "run", "dhcp_server"], { stdio: "inherit" });
-}
