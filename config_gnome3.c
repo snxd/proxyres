6e2267b8dec0537ead6d8accc35d58a6f91c845a
Sergey Markelov <sergey@solidstatenetworks.com>
2026-09-09 07:35:20 +0700

    config_gnome*: escape #undef g_free

--- config_gnome3.c
+++ config_gnome3.c
@@ -28,22 +28,22 @@ typedef struct g_proxy_config_gnome3_s {
 #if G_GNUC_CHECK_VERSION(4, 1) && GLIB_VERSION_MAX_ALLOWED >= GLIB_VERSION_2_78 && defined(G_HAVE_FREE_SIZED)
     void (*g_free_sized)(gpointer mem, size_t size);
 #endif
-    void (*g_free)(gpointer mem);
+    void (*free)(gpointer mem);  // `g_free` is a macro since Glib 2.78
     void (*g_strfreev)(gchar **str_array);
 } g_proxy_config_gnome3_s;
 
 g_proxy_config_gnome3_s g_proxy_config_gnome3;
 
-#ifdef g_free
-#  undef g_free
-#  if G_GNUC_CHECK_VERSION(4, 1) && GLIB_VERSION_MAX_ALLOWED >= GLIB_VERSION_2_78 && defined(G_HAVE_FREE_SIZED)
-#    define g_free(mem)                                                                  \
-        (__builtin_object_size((mem), 0) != ((size_t)-1))                                \
-            ? (g_proxy_config_gnome3.g_free_sized)(mem, __builtin_object_size((mem), 0)) \
-            : (g_proxy_config_gnome3.g_free)(mem)
-#  else
-#    define g_free(mem) (g_proxy_config_gnome3.g_free)(mem)
-#  endif
+#if G_GNUC_CHECK_VERSION(4, 1) && GLIB_VERSION_MAX_ALLOWED >= GLIB_VERSION_2_78 && defined(G_HAVE_FREE_SIZED)
+// This cannot be a dispatching function. Cause: `__builtin_object_size(mem, 0)`
+// must run at the call site, otherwise the compiler would not recover the
+// allocation size at .
+#  define glib_free(mem)                                                             \
+      (__builtin_object_size((mem), 0) != ((size_t)-1))                              \
+          ? g_proxy_config_gnome3.g_free_sized(mem, __builtin_object_size((mem), 0)) \
+          : g_proxy_config_gnome3.free(mem)
+#else
+#  define glib_free(mem) g_proxy_config_gnome3.free(mem)
 #endif
 
 static bool proxy_config_gnome3_is_mode(const char *mode) {
@@ -56,7 +56,7 @@ static bool proxy_config_gnome3_is_mode(const char *mode) {
     char *system_mode = g_proxy_config_gnome3.g_settings_get_string(settings, "mode");
     if (system_mode) {
         equal = strcmp(system_mode, mode) == 0;
-        g_free(system_mode);
+        glib_free(system_mode);
     }
     g_proxy_config_gnome3.g_object_unref(settings);
     return equal;
@@ -78,7 +78,7 @@ char *proxy_config_gnome3_get_auto_config_url(void) {
     if (url) {
         if (*url)
             auto_config_url = strdup(url);
-        g_free(url);
+        glib_free(url);
     }
     g_proxy_config_gnome3.g_object_unref(settings);
     return auto_config_url;
@@ -123,7 +123,7 @@ char *proxy_config_gnome3_get_proxy(const char *scheme) {
                 snprintf(proxy, max_proxy, "%s:%" PRIu32 "", host, port);
         }
 
-        g_free(host);
+        glib_free(host);
     }
     g_proxy_config_gnome3.g_object_unref(settings);
     return proxy;
@@ -177,8 +177,8 @@ bool proxy_config_gnome3_global_init(void) {
         goto gnome3_init_error;
 
     // Glib functions
-    (g_proxy_config_gnome3.g_free) = (void (*)(gpointer))dlsym(g_proxy_config_gnome3.glib_module, "g_free");
-    if (!g_proxy_config_gnome3.g_free)
+    g_proxy_config_gnome3.free = (void (*)(gpointer))dlsym(g_proxy_config_gnome3.glib_module, "g_free");
+    if (!g_proxy_config_gnome3.free)
         goto gnome3_init_error;
 #if G_GNUC_CHECK_VERSION(4, 1) && GLIB_VERSION_MAX_ALLOWED >= GLIB_VERSION_2_78 && defined(G_HAVE_FREE_SIZED)
     g_proxy_config_gnome3.g_free_sized =
